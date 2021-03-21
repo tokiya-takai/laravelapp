b914a4d3fb7c929765e22825da0bb916468f0b63
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-21 15:18:47 +0900

    ルートパラメータをテンプレートに渡す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,8 +7,11 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index() {
-        $data = ['msg'=>'これはコントローラから渡されたメッセージです。'];
+    public function index($id='zero') {
+        $data = [
+            'msg'=>'これはコントローラから渡されたメッセージです。',
+            'id'=>$id
+        ];
         return view('hello.index', $data);
     }
 }
