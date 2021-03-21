8f90a58e6a03fe91d8399c52f1780488bf87f206
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-21 20:41:11 +0900

    @ifディレクティブを使用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,17 +8,10 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index() {
-        $data = [
-            'msg'=>'名前を入力',
-        ];
-        return view('hello.index', $data);
+        return view('hello.index', ['msg'=>'']);
     }
 
     public function post(Request $request){
-        $msg = $request->msg;
-        $data = [
-            'msg'=>'こんにちは、' . $msg . 'さん！' ,
-        ];
-        return view('hello.index', $data);
+        return view('hello.index', ['msg'=>$request->msg]);
     }
 }
