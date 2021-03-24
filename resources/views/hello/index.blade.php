7da07edc72e58a2c31da98e15a7e6f3cc18a5444
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-24 09:11:17 +0900

    継承レイアウト

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,18 +1,17 @@
-<html>
-<head>
-  <title>Hello/Index</title>
-  <style>
-    body {font-size:16pt; color:#999;}
-    h1 { font-size: 100pt; text-align: right; color: #f6f6f6; margin: -50px 0 -100px 0;}
-  </style>
-</head>
-<body>
-  <h1>Blade/Index</h1>
-  <p>&#064;foreachディレクティブの例</p>
-  <ol>
-    @foreach($data as $item)
-    <li>{{$item}}</li>
-    @endforeach
-  </ol>
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです。</p>
+  <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+  copyright 2020 tuyano.    
+@endsection
