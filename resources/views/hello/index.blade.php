513bc9af92602ecc8e9beeb7ecadffb806b8309d
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-24 09:44:01 +0900

    サブビューを読み込む

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,16 +10,7 @@
 @section('content')
   <p>ここが本文のコンテンツです。</p>
   <p>必要なだけ記述できます。</p>
-
-  @component('components.message')
-      @slot('msg_title')
-          CAUTION!
-      @endslot
-
-      @slot('msg_content')
-          これはメッセージの表示です。
-      @endslot
-  @endcomponent
+  @include('components.message',['msg_title'=>'OK', 'msg_content'=>'サブビュー'])
 @endsection
 
 @section('footer')
