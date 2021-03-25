e299911aa331ddd44fbf1d117722477c0908b4ed
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-25 13:03:13 +0900

    ミドルウェアグループスへの登録

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index');
+Route::get('hello', 'HelloController@index')
+    ->middleware('hello');
 Route::post('hello', 'HelloController@post');
