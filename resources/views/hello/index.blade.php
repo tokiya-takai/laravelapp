17ead592a6c9c6916c93f369fa24ccd23e590d73
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-27 21:29:29 +0900

    エラーメッセージを個別で取得

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,24 +10,25 @@
 @section('content')
   <p>{{ $msg }}</p>
   @if (count($errors) > 0)
-  <div>
-    <ul>
-      <!--バリエーションエラーの場合に生成される$errorsを全て取り出す-->
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>        
-      @endforeach
-    </ul>
-  </div>      
+  <p>入力に問題があります。再入力してください。</p>
   @endif
   <form action="/hello" method="post">
-    <table>
-      @csrf
-      <!-- oldメソッドによって、現在の値が設定される前の値を返す -->
-      <tr><th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
-      <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
-      <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
-      <tr><th></th><td><input type="submit" value="send"></td></tr>
-    </table>
+  <table>
+    @csrf
+    @if ($errors->has('name'))
+    <tr><th>ERROR</th><td>{{  $errors->first('name') }}</td></tr>
+    @endif
+    <tr><th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
+    @if ($errors->has('mail'))
+    <tr><th>ERROR</th><td>{{ $errors->first('mail') }}</td></tr>
+    @endif
+    <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
+    @if ($errors->has('age'))
+    <tr><th>ERROR</th><td>{{ $errors->first('age') }}</td></tr>        
+    @endif
+    <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
+    <tr><th></th><td><input type="submit" value="send"></td></tr>
+  </table>
   </form>
 @endsection
 
