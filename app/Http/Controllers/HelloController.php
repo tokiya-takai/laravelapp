61003a025b90a4786f23db9b3b720faff1d47344
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-27 18:22:59 +0900

    バリエーションの設定、エラー文の表示、入力した値の保持

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,6 +8,18 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        return view('hello.index');
+        return view('hello.index', ['msg'=>'フォームを入力：']);
+    }
+
+    public function post(Request $request)
+    {
+        $validate_rule = [
+            'name' => 'required',//入力必須
+            'mail' => 'email',//mail形式
+            'age' => 'numeric|between:0, 150'//数値であり、0~150の範囲
+        ];
+        $this->validate($request, $validate_rule);//validateメソッドは、自動で処理をし、
+        //不正確な場合は残りの処理(以下のreturn)を実行せずにフォームページを表示するレスポンスが生成される。
+        return view('hello.index', ['msg'=>'正しく入力されました!']);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,9 +8,27 @@
 @endsection
 
 @section('content')
-  <p>ここが本文のコンテンツです。</p>
-  <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-  <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
+  <p>{{ $msg }}</p>
+  @if (count($errors) > 0)
+  <div>
+    <ul>
+      <!--バリエーションエラーの場合に生成される$errorsを全て取り出す-->
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>        
+      @endforeach
+    </ul>
+  </div>      
+  @endif
+  <form action="/hello" method="post">
+    <table>
+      @csrf
+      <!-- oldメソッドによって、現在の値が設定される前の値を返す -->
+      <tr><th>name: </th><td><input type="text" name="name" value="{{ old('name') }}"></td></tr>
+      <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></tr>
+      <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></tr>
+      <tr><th></th><td><input type="submit" value="send"></td></tr>
+    </table>
+  </form>
 @endsection
 
 @section('footer')
