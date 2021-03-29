63b4ddc17857bde9516acfd4eae5cb2bba3e3b29
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-29 14:44:29 +0900

    オリジナルバリデータの作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,38 +10,13 @@ use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
-    public function index(Request $request) {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if ($validator->fails()) {
-            $msg = 'クエリに問題があります。';
-        } else {
-            $msg = 'ID/PASSを受け付けました。フォームを入力してください。';
-        }
-        return view('hello.index', ['msg'=>$msg,]);
+    public function index(Request $request)
+    {
+        return view('hello.index', ['msg'=>'フォームを入力してください。']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0, 150',
-        ];
-        $messages = [
-            'name.required' => '名前は必ず入力してください。',
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.numeric' => '年齢を整数で記入してください。',
-            'age.between' => '年齢は0〜150の間で入力してください。',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages);
-        if ($validator->fails()){
-            return redirect('/hello')
-                ->withErrors($validator)
-                ->withInput();
-        }
-        return view('hello.index', ['msg'=>'正しく入力されました!']);
-        }
+        return view('hello.index', ['msg'=>'正しく入力されました！']);
+    }
 }
--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -22,7 +22,7 @@ class HelloRequest extends FormRequest
         return [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|between:0, 150',
+            'age' => 'numeric|hello',
         ];
     }
     public function messages()
@@ -31,7 +31,7 @@ class HelloRequest extends FormRequest
             'name.required' => '名前は必ず入力してください。',
             'mail.email' => 'メールアドレスが必要です。',
             'age.numeric' => '年齢を整数で記入してください。',
-            'age.between' => '年れは0〜150の間で入力してください。',
+            'age.hello' => 'Hello!入力は偶数のみ受け付けます。',
         ];
     }
 }
