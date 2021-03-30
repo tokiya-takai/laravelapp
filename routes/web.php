cbf90b1c01276f202fbc1e59f2ecce9f7314236b
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-30 22:03:19 +0900

    SQLを使用したCRUD操作

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\HelloMiddleware;
 
@@ -20,3 +21,9 @@ Route::get('/', function () {
 
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
