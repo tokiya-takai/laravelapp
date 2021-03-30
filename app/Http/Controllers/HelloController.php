a9c7576356b7bc0260c1d71c9f5e1019c2f7b06f
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-30 21:06:51 +0900

    DBクラスとパラメータ結合

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,12 +7,20 @@ use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Dotenv\Validator as DotenvValidator;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['msg'=>'フォームを入力してください。']);
+        if (isset($request->id))
+        {
+            $param = ['id' => $request->id];
+            $items = DB::select('select * from people where id = :id', $param);
+        } else {
+            $items = DB::select('select * from people');
+        }
+        return view('hello.index', ['items'=> $items]);
     }
 
     public function post(HelloRequest $request)
