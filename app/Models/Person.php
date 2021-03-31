06a9832f3af2897fdb34cd82dc8820e6b7f94d12
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-31 18:16:13 +0900

    ローカルスコープの利用

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -13,4 +13,19 @@ class Person extends Model
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
     }
+
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
+
+    public function scopeAgeGreaterThan($query, $n)
+    {
+        return $query->where('age', '>=', $n);
+    }
+
+    public function scopeAgeLessThan($query, $n)
+    {
+        return $query->where('age', '<=', $n);
+    }
 }
