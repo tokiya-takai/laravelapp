60987649b92712fdc2e38f3c5089aa64da013cef
tokiya-takai <t.orca.dolphin@gmail.com>
2021-03-31 13:14:07 +0900

    マイグレーションファイルとシーディング

---
+++ database/seeders/PeopleTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PeopleTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $param = [
+            'name' => 'taro',
+            'mail' => 'taro@gmail.com',
+            'age' => 12,
+        ];
+        DB::table('people')->insert($param);
+
+        $param = [
+            'name' => 'hanako',
+            'mail' => 'hanako@gmail.com',
+            'age' => 34,
+        ];
+        DB::table('people')->insert($param);
+
+        $param = [
+            'name' => 'sachiko',
+            'mail' => 'sachiko@gmail.com',
+            'age' => 56,
+        ];
+        DB::table('people')->insert($param);
+    }
+}
