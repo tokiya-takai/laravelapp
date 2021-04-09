f20038f00a4654b76ad1eb7e3480eaa6db8c9810
tokiya-takai <t.orca.dolphin@gmail.com>
2021-04-09 15:50:10 +0900

    hasManyを使う

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -25,8 +25,8 @@ class Person extends Model
     }
 
     #リレーション
-    public function board()
+    public function boards()
     {
-        return $this->hasOne('App\Models\Board');
+        return $this->hasMany('App\Models\Board');
     }
 }
